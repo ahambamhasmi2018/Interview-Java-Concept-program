d239a2c29851bb296a961004a818db5237381e2b
ahambamhasmi2018 <ahambamhasmi@gmail.com>
2019-10-23 03:16:14 +0530

    Char Count of String
    Change-Id: I9df6d5af528e45a087bd5180f0cb4d996a883e77
    Signed-off-by: ahambamhasmi2018 <ahambamhasmi@gmail.com>

---
+++ JavaDemo/src/CollectionSetList/EachCharCountInString.java
@@ -0,0 +1,48 @@
+package CollectionSetList;
+
+import java.util.HashMap;
+
+public class EachCharCountInString {
+	private static void characterCount(String inputString)
+    {
+        //Creating a HashMap containing char as a key and occurrences as a value
+  
+        HashMap<Character, Integer> charCountMap = new HashMap<Character, Integer>();
+  
+        //Converting given string to char array
+  
+        char[] strArray = inputString.toCharArray();
+  
+        //checking each char of strArray
+  
+        for (char c : strArray)
+        {
+            if(charCountMap.containsKey(c))
+            {
+                //If char 'c' is present in charCountMap, incrementing it's count by 1
+  
+                charCountMap.put(c, charCountMap.get(c)+1);
+            }
+            else
+            {
+                //If char 'c' is not present in charCountMap,
+                //putting 'c' into charCountMap with 1 as it's value
+  
+                charCountMap.put(c, 1);
+            }
+        }
+         
+        //Printing inputString and charCountMap 
+  
+        System.out.println(inputString+" : "+charCountMap);
+    }
+  
+    public static void main(String[] args)
+    {
+       characterCount("Java J2EE Java JSP J2EE");
+  
+       characterCount("All Is Well");
+  
+       characterCount("Done And Gone");
+    }
+}
